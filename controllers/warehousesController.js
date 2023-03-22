68dcb54ba2617643f8c59877888aff5272b3012e
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2023-03-22 17:07:27 -0400

    add post req for new warehouse in warehouse controller

--- controllers/warehousesController.js
+++ controllers/warehousesController.js
@@ -10,3 +10,32 @@ exports.index = (_req, res) => {
       res.status(400).send(`Error retrieving Warehouses: ${err}`)
     );
 };
+
+exports.addWarehouse = (req, res) => {
+  // Validate the request body for required data
+  if (
+    !req.body.warehouse_name ||
+    !req.body.address ||
+    !req.body.city ||
+    !req.body.country ||
+    !req.body.contact_name ||
+    !req.body.contact_position ||
+    !req.body.contact_phone ||
+    !req.body.contact_email
+  ) {
+    return res
+      .status(400)
+      .send(
+        "Please make sure to provide name, manager, address, phone and email fields in a request"
+      );
+  }
+
+  knex("warehouse")
+    .insert(req.body)
+    .then((data) => {
+      // For POST requests we need to respond with 201 and the location of the newly created record
+      const newWarehouseURL = `/warehouses/${data[0]}`;
+      res.status(201).location(newWarehouseURL).send(newWarehouseURL);
+    })
+    .catch((err) => res.status(400).send(`Error creating Warehouse: ${err}`));
+};
