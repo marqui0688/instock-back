ad3f0f52af038e6ae1397b7a84b0b4fedc885cdf
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2023-03-22 18:32:07 -0400

    update add  inventory put  endpoint for add items

--- routes/inventoriesRoute.js
+++ routes/inventoriesRoute.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const inventoriesController = require("../controllers/inventoriesController");
 
 router.route("/").get(inventoriesController.index);
-// router.route("/").post(inventoriesController.???);
+
+router.route("/").post(inventoriesController.addInventory);
 
 module.exports = router;
