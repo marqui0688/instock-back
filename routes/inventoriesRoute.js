d0329089561968a1a911bc98e6f3a9cd3c507a0c
kakun45 <email4blackkat@gmail.com>
2023-03-23 10:47:50 -0400

    added getSingleInventory

--- routes/inventoriesRoute.js
+++ routes/inventoriesRoute.js
@@ -3,6 +3,9 @@ const inventoriesController = require("../controllers/inventoriesController");
 
 router.route("/").get(inventoriesController.index);
 
+// Note: The warehouse name must also be included in the response:
+router.route("/:inventoryId").get(inventoriesController.getSingleInventory);
+
 router.route("/").post(inventoriesController.addInventory);
 
 module.exports = router;
